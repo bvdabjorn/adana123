57b60c700ad5475b2490245f27e5654ab3342983
bvdabjorn <bvda.bjorn@icloud.com>
2020-03-10 10:04:10 +0100

    adfa

--- main.R
+++ main.R
@@ -7,3 +7,4 @@ data_test <- read.csv('test.csv', header = TRUE)
 
 #Create dataframe with all entries that have missing values (26 missing data entries based on original dataset)
 missing_data_train <- data_train[rowSums(is.na(data_train)) > 0,]
+
--- preprocessing.py
+++ preprocessing.py
@@ -11,11 +11,41 @@ new_cpu = data_train['cpu'].str.split(' ', n=1, expand=True)
 data_train['cpu_brand'] = new_cpu[0]
 data_train['cpu_type_name'] = new_cpu[1]
 
+#Split up the gpu column
 new_gpu = data_train['gpu'].str.split(' ', n=1, expand=True)
 data_train['gpu_brand'] = new_gpu[0]
 data_train['gpu_type'] = new_gpu[1]
 
+#Extracting only the os details 
+#Heb hier voor makkelijkste oplossing gekozen voor deze dataset; niet echt veralgemeenbaar maar werkt voor deze dataset
 length_data_frame = len(data_train['os_details'])
-for i in range(0,length_data_frame+1):
+new_os = list()
+for i in range(0,length_data_frame):
     if data_train['os_details'][i][0] == 'W':
-        data_train['os_details'][i].str.split(' ', n=1, expand=True)
+        new_os.append(data_train['os_details'][i][8:])
+    elif data_train['os_details'][i][0] == 'C':
+        new_os.append(data_train['os_details'][i][0:])
+    elif data_train['os_details'][i][0] == 'O':
+        new_os.append(data_train['os_details'][i][5:])
+    elif data_train['os_details'][i][0] == 'M':
+        new_os.append(data_train['os_details'][i][6:])
+    elif data_train['os_details'][i][0] == 'A':
+        new_os.append(data_train['os_details'][i][8:])
+    else:
+        new_os.append(data_train['os_details'][i][0:])
+        
+data_train['os_details_2'] = new_os
+
+#Extracting cpu-details
+"""data_train.apply(lambda x: x['cpu_details'].split(' ')[x['cpu_details'].split(' ').index('GHZ')-2].split('-')[1] \
+       if '-' in x['cpu_details'].split(' ')[x['cpu_details'].split(' ').index('GHZ')-2] \
+       else x['cpu_details'].split(' ')[x['cpu_details'].split(' ').index('GHZ')-2],axis=1)"""
+
+cpu_details_2 = list()
+if '-' in data_train['cpu_details'].split(' ')[data_train['cpu_details'].split(' ').index('GHZ')-2] :
+    cpu_details_2.append(data_train['cpu_details'].split(' ')[data_train['cpu_details'].split(' ').index('GHZ')-2].split('-')[1])
+elif pd.isnull(data_train['cpu_details'].split(' ')[data_train['cpu_details'].split(' ').index('GHZ')-2]): 
+    cpu_details_2.append(None)
+else:
+    cpu_details_2.append(data_train['cpu_details'].split(' ')[data_train['cpu_details'].split(' ').index('GHZ')-2])
+    
