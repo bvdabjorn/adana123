c10f221331ecb2eca8ca0e2dfd20496dc10d4bcb
r0664975 <61828432+r0664975@users.noreply.github.com>
2020-03-24 14:51:09 +0100

    Preprocessing fix 1

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from fuzzywuzzy import fuzz
 
 ## Edit Print options
 desired_width = 500
@@ -8,9 +9,15 @@ pd.set_option('display.max_columns', 30)
 
 
 data_train = pd.read_csv('train.csv', keep_default_na = True)
+# Load data
+data_train = pd.read_csv('/Users/bjrn/Documents/GitHub/adana123/train.csv', keep_default_na=True)
+cpu_data = pd.read_csv('/Users/bjrn/Documents/GitHub/adana123/CPU_Benchmark.csv', sep=';')
+gpu_data = pd.read_csv('/Users/bjrn/Documents/GitHub/adana123/GPU_Benchmark.csv', sep=';')
 
 #Make every entry uppercase
 data_train = data_train.apply(lambda x: x.astype(str).str.upper())
+cpu_data = cpu_data.apply(lambda x: x.astype(str).str.upper())
+gpu_data = gpu_data.apply(lambda x: x.astype(str).str.upper())
 
 ## Split up the cpu column
 # Create 2 new columns: CPU Brand and CPU Type
@@ -24,7 +31,7 @@ new_gpu = data_train['gpu'].str.split(' ', n=1, expand=True)
 data_train['gpu_brand'] = new_gpu[0]
 data_train['gpu_type'] = new_gpu[1]
 
-#Extracting only the os details 
+#Extracting only the os details
 #Heb hier voor makkelijkste oplossing gekozen voor deze dataset; niet echt veralgemeenbaar maar werkt voor deze dataset
 length_data_frame = len(data_train['os_details'])
 new_os = list()
@@ -41,7 +48,7 @@ for i in range(0,length_data_frame):
         new_os.append(data_train['os_details'][i][8:])
     else:
         new_os.append(data_train['os_details'][i][0:])
-        
+
 data_train['os_details_2'] = new_os
 
 
@@ -63,6 +70,19 @@ for i in range(0, length_data_frame):
 
 data_train['cpu_details_2'] = cpu_details_2
 
+# Linking cpu benchmark results to dataframe
+data_train['cpu_benchmark'] = 'NAN'
+for i in range(len(data_train['cpu_details_2'])):
+    for j in range(len(cpu_data['cpu name'])):
+        if fuzz.ratio(data_train['cpu_details_2'][i], cpu_data['cpu name'][j]) > 95:
+            data_train['cpu_benchmark'][i] = cpu_data['passmark cpu score'][j]
+
+ # Linking gpu benchmark results to dataframe
+data_train['gpu_benchmark'] = 'NAN'
+for i in range(len(data_train['gpu_type'])):
+    for j in range(len(gpu_data['gpu name'])):
+        if fuzz.ratio(data_train['gpu_type'][i], gpu_data['gpu name'][j]) > 95:
+            data_train['gpu_benchmark'][i] = gpu_data['gpu passmark score'][j]
 
 
 ## Difference between Max and Min prices:
@@ -75,3 +95,4 @@ data_train['diff_price'] = diff_price
 data_train.to_csv("/Users/Simon/Documents/GitHub/adana123/datafile2.csv")
 
 # print(data_train)
+
