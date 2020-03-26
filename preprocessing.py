b5aebdee7fb61f04572bf111b13ed4ab0aadd9bc
bvdabjorn <bvda.bjorn@icloud.com>
2020-03-26 12:24:06 +0100

    dfdf

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import numpy as np
-
-## Edit Print options
-desired_width = 500
-pd.set_option('display.width', desired_width)
-pd.set_option('display.max_columns', 30)
+from fuzzywuzzy import fuzz
 
 
-data_train = pd.read_csv('train.csv', keep_default_na = True)
+# Load data
+data_train = pd.read_csv('/Users/bjrn/Documents/GitHub/adana123/train.csv', keep_default_na=True)
+cpu_data = pd.read_csv('/Users/bjrn/Documents/GitHub/adana123/CPU_Benchmark.csv', sep=';')
+gpu_data = pd.read_csv('/Users/bjrn/Documents/GitHub/adana123/GPU_Benchmark.csv', sep=';')
 
 #Make every entry uppercase
 data_train = data_train.apply(lambda x: x.astype(str).str.upper())
+cpu_data = cpu_data.apply(lambda x: x.astype(str).str.upper())
+gpu_data = gpu_data.apply(lambda x: x.astype(str).str.upper())
 
 ## Split up the cpu column
 # Create 2 new columns: CPU Brand and CPU Type
@@ -24,7 +25,7 @@ new_gpu = data_train['gpu'].str.split(' ', n=1, expand=True)
 data_train['gpu_brand'] = new_gpu[0]
 data_train['gpu_type'] = new_gpu[1]
 
-#Extracting only the os details 
+#Extracting only the os details
 #Heb hier voor makkelijkste oplossing gekozen voor deze dataset; niet echt veralgemeenbaar maar werkt voor deze dataset
 length_data_frame = len(data_train['os_details'])
 new_os = list()
@@ -41,7 +42,7 @@ for i in range(0,length_data_frame):
         new_os.append(data_train['os_details'][i][8:])
     else:
         new_os.append(data_train['os_details'][i][0:])
-        
+
 data_train['os_details_2'] = new_os
 
 
@@ -59,11 +60,33 @@ for i in range(0, length_data_frame):
         else:
             cpu_details_2.append(data_train['cpu_details'][i].split(' ')[data_train['cpu_details'][i].split(' ').index('GHZ')-2])
     else:
-        cpu_details_2.append('NAN')
-
+        cpu_details_2.append(np.nan)
+        
 data_train['cpu_details_2'] = cpu_details_2
+        
+cpu_GHZ = list()
+for i in range(0, length_data_frame):
+    if 'GHZ' in data_train['cpu_details'][i].split(' '):
+        GHZ = data_train['cpu_details'][i].split(' ')[data_train['cpu_details'][i].split(' ').index('GHZ')-1]
+        cpu_GHZ.append(GHZ)
+    else:
+        cpu_GHZ.append(np.nan)
 
+data_train['cpu_GHZ'] = cpu_GHZ
 
+# Linking cpu benchmark results to dataframe
+data_train['cpu_benchmark'] = np.NaN
+for i in range(len(data_train['cpu_details_2'])):
+    for j in range(len(cpu_data['cpu name'])):
+        if str(data_train['cpu_details_2'][i]) in str(cpu_data['cpu name'][j]):
+            data_train['cpu_benchmark'][i] = cpu_data['passmark cpu score'][j]
+
+ # Linking gpu benchmark results to dataframe
+data_train['gpu_benchmark'] = np.nan
+for i in range(len(data_train['gpu_type'])):
+    for j in range(len(gpu_data['gpu name'])):
+        if str(data_train['gpu_type'][i]) in str(gpu_data['gpu name'][j]):
+            data_train['gpu_benchmark'][i] = gpu_data['gpu passmark score'][j]
 
 ## Difference between Max and Min prices:
 diff_price = list()
@@ -72,6 +95,5 @@ for i in range(0,length_data_frame):
 
 data_train['diff_price'] = diff_price
 # data_train.to_csv("/Users/bjrn/Documents/GitHub/adana123/datafile2.csv")
-data_train.to_csv("/Users/Simon/Documents/GitHub/adana123/datafile2.csv")
+data_train.to_csv("/Users/bjrn/Documents/GitHub/adana123/datafile2.csv")
 
-# print(data_train)
