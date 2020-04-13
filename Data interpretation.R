8639aa9f0a82cb03dd21f8af1e376456ee9acfec
r0664975 <61828432+r0664975@users.noreply.github.com>
2020-04-13 12:04:41 +0200

    Report
    Enkel nog missing values, correlation en plots van enkele die niet in kleine trainingset zaten

--- Data interpretation.R
+++ Data interpretation.R
@@ -26,13 +26,13 @@ attach(data_train)
 
 ggplot.bar <- function(DT, variable, xlab){
   ggplot(data = DT, aes(as.factor(variable))) + theme_bw() + 
-    geom_bar(aes(y = (..count..)/sum(..count..)), col = blue, fill = blue, alpha = 0.75,) +
+    geom_bar(aes(y = (..count..)/sum(..count..) ), col = blue, fill = blue, alpha = 0.75,) +
     labs(y= "Abs frequency", x = xlab) +
     ggtitle(paste(xlab,"frequency plot"))
 }
 ggplot.hist <- function(DT, variable, xlab){
   ggplot(data = DT, aes(variable)) + theme_bw() + 
-    geom_histogram(aes(y = (..count..)/sum(..count..)), col = blue, fill = blue, alpha = 0.75, bins = 20) +
+    geom_histogram(aes(y = (..count..)/sum(..count..) ), col = blue, fill = blue, alpha = 0.75, bins = 20) +
     labs(y= "Abs frequency", x = xlab) +
     ggtitle(paste(xlab,"frequency plot"))
 }
@@ -46,10 +46,12 @@ ggplot.bar(data_train, data_train$screen_surface, "screen_surface")
 ggplot.bar(data_train, data_train$touchscreen, "touchscreen")
 ggplot.bar(data_train, data_train$cpu, "cpu")
 ggplot.bar(data_train, data_train$cpu_GHZ, "cpu_GHZ")
-ggplot.bar(data_train, data_train$cpu_benchmark, "cpu_benchmark")
+ggplot.bar(data_train, data_train$cpu_core, "cpu_core")
+ggplot.bar(data_train, data_train$threading, "threading")
+ggplot.hist(data_train, data_train$cpu_benchmark, "cpu_benchmark")
 ggplot.bar(data_train, data_train$discrete_gpu, "discrete_gpu")
 ggplot.bar(data_train, data_train$gpu, "gpu")
-ggplot.bar(data_train, data_train$gpu_benchmark, "gpu_benchmark")
+ggplot.hist(data_train, data_train$gpu_benchmark, "gpu_benchmark")
 ggplot.bar(data_train, data_train$os, "os")
 ggplot.bar(data_train, data_train$os_details, "os_details")
 ggplot.bar(data_train, data_train$ram, "ram")
@@ -58,14 +60,19 @@ ggplot.bar(data_train, data_train$storage, "storage")
 ggplot.hist(data_train, data_train$weight, "weight")
 ggplot.bar(data_train, data_train$cpu_brand, "cpu_brand")
 ggplot.bar(data_train, data_train$cpu_type_name, "cpu_type_name")
-ggplot.bar(data_train, data_train$gpu_brand, "cpu_brand")
+ggplot.bar(data_train, data_train$gpu_brand, "gpu_brand")
 ggplot.bar(data_train, data_train$gpu_type, "gpu_type")
 ggplot.bar(data_train, data_train$os_details_2, "os_details_2")
 ggplot.bar(data_train, data_train$cpu_details_2, "cpu_details_2")
 ggplot.bar(data_train, data_train$pixel, "pixel")
 ggplot.bar(data_train, data_train$detachable_keyboard, "detachable_keyboard")
 
-gg
+#cpu_core frequency plot manueel om x-as in logische volgorde te hebben
+ggplot(data = data_train, aes(as.factor(data_train$cpu_core))) + theme_bw() + 
+  geom_bar(aes(y = (..count..)/sum(..count..), x = c("","DUAL-CORE","QUAD-CORE","HEXA-CORE","OCTA-CORE")  ), col = blue, fill = blue, alpha = 0.75,) +
+  labs(y= "Abs frequency", x = xlab) +
+  ggtitle("cpu_core frequency plot")
+
 
 ggplot.point <- function(DT, variable, xlab){
   ggplot(data_train, aes(variable, min_price)) +
